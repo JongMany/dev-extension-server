c1cc14c942e954619625e8441a9776bd76c0c4ae
JongMany <blackberry1114@naver.com>
2024-05-24 03:26:27 +0900

    fix: preflight 추가

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,65 @@ import { Request, Response, json } from 'express';
 const whiteList = ['http://43.203.55.144', 'http://localhost:3000'];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Pre flight request
+  // // Pre flight request
+  // app.use((req: Request, res: Response, next) => {
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Origin', req.headers.origin);
+  //     res.header(
+  //       'Access-Control-Allow-Methods',
+  //       'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //     );
+  //     res.header('Access-Control-Allow-Credentials', 'true');
+  //     res.header(
+  //       'Access-Control-Allow-Headers',
+  //       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  //     );
+  //     return res.sendStatus(200);
+  //   }
+  //   next();
+  // });
+
+  // app.enableCors({
+  //   // origin: whiteList,
+  //   // origin: whiteList,
+  //   // origin: true,
+  //   origin: (origin, callback) => {
+  //     if (!origin || whiteList.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   // origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+
+  //   credentials: true,
+  //   // allowedHeaders:
+  //   //   'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  // });
+
+  // Logging middleware to debug CORS issues
+  app.use((req: Request, res: Response, next) => {
+    console.log(`Request from origin: ${req.headers.origin}`);
+    console.log(`Request method: ${req.method}`);
+    next();
+  });
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || whiteList.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  });
+
+  // Manually handle OPTIONS requests
   app.use((req: Request, res: Response, next) => {
     if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -20,31 +78,19 @@ async function bootstrap() {
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization',
       );
-      return res.sendStatus(200);
+      return res.sendStatus(204);
     }
     next();
   });
-
-  app.enableCors({
-    // origin: whiteList,
-    // origin: whiteList,
-    // origin: true,
-    origin: (origin, callback) => {
-      if (!origin || whiteList.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    // origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-
-    credentials: true,
-    // allowedHeaders:
-    //   'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  });
   app.use(cookieParser());
   app.use(json());
+
+  // Logging
+  app.use((req: Request, res: Response, next) => {
+    console.log(`Request from origin: ${req.headers.origin}`);
+    console.log(`Request method: ${req.method}`);
+    next();
+  });
 
   await app.listen(8080);
 }
