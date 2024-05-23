0538b491d4f667b6577f173adb83e4098a2b3392
JongMany <blackberry1114@naver.com>
2024-05-24 03:03:26 +0900

    fix: cors 추가

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,20 @@ import * as cookieParser from 'cookie-parser';
 
 import { json } from 'express';
 
-const whiteList = ['http://43.203.55.144:80', 'http://localhost:3000', '*'];
+const whiteList = ['http://43.203.55.144', 'http://localhost:3000'];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: whiteList,
-    origin: 'http://43.203.55.144:80',
+    // origin: whiteList,
     // origin: true,
-    /* (origin, callback) => {
-      console.log('origin', origin);
-      if (whiteList.indexOf(origin) !== -1) {
+    origin: (origin, callback) => {
+      if (!origin || whiteList.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
       }
-    }, */
+    },
     // origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 
